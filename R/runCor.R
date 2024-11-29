2f8a711767fa49f3b13fbfbd6938fd8d59a05daf
correadarosaj <joelrosa@uol.com.br>
2024-11-28 19:16:16 -0500

    udat

--- R/runCor.R
+++ R/runCor.R
@@ -1,6 +1,9 @@
-#-----------------------------------------------
-# correlation function
-#-----------------------------------------------
+#  This will extract a column of a data frame or tibble and convert into a vector
+#' The function outputs a vector from a data frame or a tibble
+#' @description The function extract a column from a data frame and converts it into a vector
+#' @param data a data frame of tibble
+#' @param var the name of the column to be extracted
+#' @examples
 runCor = function(varx,vary,dat = newdata){
 
   tempdata = dat %>%
