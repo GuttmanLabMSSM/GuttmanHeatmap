181a456681ff6adec778c063a4249ddc91aa8fb7
Joel Correa da Rosa <joelrosa@uol.com.br>
2025-02-19 13:30:29 -0500

    Update of the doTab function

--- R/doTab.R
+++ R/doTab.R
@@ -1,20 +1,19 @@
-
 doTab<-function(fdrsx,coefsx){
+  # loading libraries that are needed
   require(stringr)
   require(weights)
 
-  p.cuts.stars=c(0.001,0.01, 0.05, 0.1,1)
-  p.symbols.stars= c('***',"**", "*", "+","  ")
+  #----------------------------------------------------------
+  # functions to be used inside this function
+  #----------------------------------------------------------
 
-  coefsx[coefsx==0]<-1e-10
-
-  fdrsx[] <- sapply(fdrsx,starmaker, p.levels =p.cuts.stars, symbols = p.symbols.stars)
-
+  # a function to convert log-fold changes into fold-changes
   transformfch <- function(lgfch) {
     fch <- sign(lgfch) * (2^abs(lgfch))
     return(fch)
   }
 
+  # a padding function just to fill some gaps
   padding<-function(x,n){
     m=ceiling(log10(abs(x)+.001))
     x<-as.character(x)
@@ -33,23 +32,46 @@ doTab<-function(fdrsx,coefsx){
     return(x)
   }
 
-
-
+  # a function to pad stars
   padstars<-function(x){
     while (nchar(x)<3){x<-paste(x,'')}
     return(x)
   }
+
+  # a function to pad spaces
+  adspaceText <- function(x, n) {
+    paste0(x,"<span style='font-size:12pt;color:black'>",' ',str_c(rep("\\-",(n-nchar(x))),collapse = ''),"</span>","\\:")
+  }
+
+  # creating the map between the p-values thresholds and stars
+  p.cuts.stars=c(0.001,0.01, 0.05, 0.1,1)
+  p.symbols.stars= c('***',
+                     paste0('**',"<span style='font-size:12pt;color:white'>",'*',"</span>",collapse = ''),
+                     paste0('*',"<span style='font-size:12pt;color:white'>",'*',"</span>","<span style='font-size:12pt;color:white'>",'*',"</span>",collapse = ''),
+                     paste0('+',"<span style='font-size:12pt;color:white'>",'*',"</span>","<span style='font-size:12pt;color:white'>",'*',"</span>",collapse = ''),
+                     paste0("<span style='font-size:12pt;color:white'>",'*',"</span>","<span style='font-size:12pt;color:white'>",'*',"</span>","<span style='font-size:12pt;color:white'>",'*',"</span>",collapse = '')
+  )
+  #"*  ", "+  ","   ")
+
+  # to avoid problems with 0 values
+  coefsx[coefsx==0]<-1e-10
+
+  # replace p-values with stars
+  fdrsx[] <- sapply(fdrsx,starmaker, p.levels =p.cuts.stars, symbols = p.symbols.stars)
+
 
   coefsx[]<-sapply(coefsx,transformfch)
   coefsx<-round(coefsx,2)
 
   ss<-rownames(coefsx)
   maxss <- max(sapply(ss, nchar))
-  adspaceText <- function(x, n) {paste0(x,"<span style='font-size:12pt;color:black'>",' ',str_c(rep("\\-",(n-nchar(x))),collapse = ''),"</span>","\\:")}
+
 
   ss <- sapply(ss, adspaceText, maxss+1)
 
   maxdigits=ceiling(log10(max(abs(coefsx))))
+
+  # a for loop to create a table with all coefficients and p-values
   Tab <- data.frame(Symbol = ss)
   for (i in c(1:ncol(coefsx))) {
     Tab[colnames(coefsx)[i]] <-
